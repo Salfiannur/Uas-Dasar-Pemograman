1cde19e1f3e2a4023ddde44d7ada5c6d36dc630d
Salfiannur <115383310+Salfiannur@users.noreply.github.com>
2023-01-07 23:26:13 +0700

    Add files via upload

---
+++ biodata.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 07 Jan 2023 pada 17.05
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `latihan_crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `biodata`
+--
+
+CREATE TABLE `biodata` (
+  `NIM` int(50) NOT NULL,
+  `Nama` varchar(50) NOT NULL,
+  `Telepon` int(50) NOT NULL,
+  `Alamat` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `biodata`
+--
+
+INSERT INTO `biodata` (`NIM`, `Nama`, `Telepon`, `Alamat`) VALUES
+(15, 'Naufal luthfianto', 81252, 'Bandulan');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `biodata`
+--
+ALTER TABLE `biodata`
+  ADD PRIMARY KEY (`NIM`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
